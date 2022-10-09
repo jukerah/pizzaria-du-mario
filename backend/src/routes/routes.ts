dc3ceed3ceb93a4e37d00b323a08d5a9a2beace3
Mario Elvio <juka_mebaj@hotmail.com>
2022-10-09 16:17:52 -0300

    feat: create category and list categories.

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -3,13 +3,20 @@ import { Router } from 'express';
 import { CreateUserController } from '../controllers/user/CreateUserController';
 import { AuthUserController } from '../services/user/AuthUserController';
 import { DetailUserController } from '../controllers/user/DetailUserController';
+import { CreateCategoryController } from '../controllers/category/CreateCategoryController';
+import { ListCategoryController } from '../controllers/category/ListCategoryController';
 
 import { isAuthenticated } from '../middlewares/isAuthenticated';
 
 const router = Router();
 
+// User
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
 router.get('/me', isAuthenticated, new DetailUserController().handle);
+
+// Category
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
+router.get('/category', isAuthenticated, new ListCategoryController().handle);
 
 export { router };
