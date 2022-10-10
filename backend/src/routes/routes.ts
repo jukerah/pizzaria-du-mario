a5a15f7d0eff906b6b977c0e6b317272fd91e550
Mario Elvio <juka_mebaj@hotmail.com>
2022-10-09 21:33:16 -0300

    feat: list products by category.

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -7,6 +7,7 @@ import { DetailUserController } from '../controllers/user/DetailUserController';
 import { CreateCategoryController } from '../controllers/category/CreateCategoryController';
 import { ListCategoryController } from '../controllers/category/ListCategoryController';
 import { CreateProductController } from '../controllers/product/CreateProductController';
+import { ListByCategoryController } from '../controllers/product/ListByCategoryController';
 
 import { isAuthenticated } from '../middlewares/isAuthenticated';
 
@@ -26,5 +27,6 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle);
 
 // Product
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
+router.get('/product', isAuthenticated, new ListByCategoryController().handle);
 
 export { router };
