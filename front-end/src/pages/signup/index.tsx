cb8588f49e6cfd49f355e2138f8b517524d89444
Mario Elvio <juka_mebaj@hotmail.com>
2022-10-16 22:47:20 -0300

    feat: signup page.

--- front-end/src/pages/signup/index.tsx
+++ front-end/src/pages/signup/index.tsx
@@ -1,28 +1,33 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../../styles/home.module.scss';
+import styles from '../../../styles/home.module.scss';
 
-import logoImg from '../../public/logo-vertical.svg';
+import logoImg from '../../../public/logo-vertical.svg';
 
-import { Input } from '../components/ui/Input';
-import { Button } from '../components/ui/Button';
+import { Input } from '../../components/ui/Input';
+import { Button } from '../../components/ui/Button';
 import Link from 'next/link';
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>Pizza du&apos;Mario - Faça seu login</title>
+        <title>Pizza du&apos;Mario - Faça seu cadastro agora!</title>
       </Head>
       <div className={styles.contrainerCenter}>
         <Image src={logoImg} alt="Logotipo pizzaria du'Mario" />
         
         <div className={styles.login}>
-          <h1>Acessar sua conta</h1>
+          <h1>Criando sua conta</h1>
 
           <form>
             <Input
               type="text"
+              placeholder="Digite seu nome"
+            />
+
+            <Input
+              type="email"
               placeholder="Digite seu email"
             />
 
@@ -35,12 +40,12 @@ export default function Home() {
               type="submit"
               loading={false}
             >
-              Entrar
+              Cadastrar
             </Button>
           </form>
 
-          <Link href="/signup">
-            <a className={styles.text}>Não possui uma conta? Cadastre-se!</a>
+          <Link href="/">
+            <a className={styles.text}>Já possui uma conta? Faça seu login!</a>
           </Link>
         </div>
       </div>
