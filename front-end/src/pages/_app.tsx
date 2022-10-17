90692cecbe8ba2afb38fb6565534a8eb89430b29
Mario Elvio <juka_mebaj@hotmail.com>
2022-10-16 22:27:01 -0300

    feat: login page.

--- front-end/src/pages/_app.tsx
+++ front-end/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import '../../styles/globals.scss';
 import { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,7 +1,47 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../../styles/home.module.scss';
+
+import logoImg from '../../public/logo-vertical.svg';
+
+import { Input } from '../components/ui/Input';
+import { Button } from '../components/ui/Button';
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Pizzaria du&apos;Mario</h1>
-    </div>
+    <>
+      <Head>
+        <title>Pizza du&apos;Mario - Faça seu login</title>
+      </Head>
+      <div className={styles.contrainerCenter}>
+        <Image src={logoImg} alt="Logotipo pizzaria du'Mario" />
+        
+        <div className={styles.login}>
+          <form>
+            <Input
+              type="text"
+              placeholder="Digite seu email"
+            />
+
+            <Input
+              type="password"
+              placeholder="Digite sua senha"
+            />
+
+            <Button
+              type="submit"
+              loading={false}
+            >
+              Acessar
+            </Button>
+          </form>
+
+          <Link href="/signup">
+            <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+          </Link>
+        </div>
+      </div>
+    </>
   )
 }
