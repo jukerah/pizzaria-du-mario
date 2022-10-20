e0a1fcda32d5445e477ab03b9082933b923ab7c9
Mario Elvio <juka_mebaj@hotmail.com>
2022-10-19 23:35:49 -0300

    feat: before opening a private route it validates if the user data is valid.

--- front-end/src/contexts/AuthContext.tsx
+++ front-end/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useState, useEffect } from 'react';
 import Router from 'next/router';
 import { destroyCookie, setCookie, parseCookies } from 'nookies';
 import { toast } from 'react-toastify';
@@ -48,6 +48,21 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [ user, setUser ] = useState<UserProps>();
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { '@nextauth.token': token } = parseCookies();
+
+    if (token) {
+      api.get('/me').then(response => {
+        const { id, name, email } = response.data;
+
+        setUser({ id, name, email });
+      })
+      .catch(() => {
+        signOut();
+      })
+    } 
+  });
 
   async function signIn({ email, password }: SignInProps) {
     try {
